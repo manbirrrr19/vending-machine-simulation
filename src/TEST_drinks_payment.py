10cd32e407c5e279a076279d225920e855263d76
ziqiong <ziqiong.22@ichat.sp.edu.sg>
2024-02-06 09:33:17 +0800

    Deleting unneccessary files

--- src/TEST_drinks_payment.py
+++
@@ -1,9 +0,0 @@
-import drinks_payment
-
-def test_check_record():
-    rfid_id = 123456789
-
-    result = drinks_payment.check_record(rfid_id)
-    test = "account created"
-
-    assert (result == test)
